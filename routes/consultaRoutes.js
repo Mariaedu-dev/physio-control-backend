eaf5664d244704334e3ce634ba6f36a7db02a3c2
Mariaedu-dev <vilarim051@gmail.com>
2026-04-24 13:49:20 -0300

    fix: recorrigindo nome do arquivo

--- routes/consultaRoutes.js
+++ routes/consultaRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/consultaController');
+const controller = require('../controllers/ConsultaController');
 
 router.post('/', controller.criarConsulta);
 router.get('/', controller.listarConsultas);
